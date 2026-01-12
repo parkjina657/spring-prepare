3e924823d3267bf26b6f752f03523e9af3a0501e
JINAPARK <parkjina657@g.hongik.ac.kr>
2026-01-12 11:47:34 +0900

    chore: Lombok을 기본생성자, 모든인수를 받는 생성자, 필수인 수를 받는 생성자

--- src/main/java/com/meta/springprepare/memo/Memo.java
+++ src/main/java/com/meta/springprepare/memo/Memo.java
@@ -1,18 +1,21 @@
 package com.meta.springprepare.memo;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.Date;
 
 @Getter
 @Setter
+//@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
 
 public class Memo {
-    private String username;
+    private final String username;
+    private final String title;
     private String contents;
     private Date createAt;
-    private Date updateAt;
+    private Date updatedAt;
 
     public void writeMemo() {
 
